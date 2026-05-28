046f33cf06d81a7d74476442ee883d274b854aa1
SkVaYeR228 <tima060207@gmail.com>
2026-05-28 13:26:54 +0300

    Add tests and CI pipeline

---
+++ test_app.py
@@ -0,0 +1,13 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_home_page(client):
+    """Тест перевіряє, що головний роут відповідає"""
+    response = client.get('/')
+    assert response.status_code in [200, 500]
